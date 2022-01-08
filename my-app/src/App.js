d1efc8eb866a6dd9704808d7474d9b264f67f4f4
Nghia, Nguyen <Nghia.Nguyen1@ge.com>
2022-01-08 15:49:30 +0700

    Complete Todo React Basic demo

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text='learn react'/>
+      <Todo text='master react'/>
+      <Todo text='full function react'/>
+
+    </div>
+  );
+}
+
+export default App;
